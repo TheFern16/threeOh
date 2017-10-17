db860e15d4ca96ea685a93ff069f802fa3124922
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-17 10:39:41 -0700

    export simple artist schema

--- db/schema.js
+++ db/schema.js
@@ -1,2 +1,12 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('hiphop', 'root', '');
+
+var Artist = db.define('Artist', {
+  artistName: Sequelize.STRING
+});
+
+Artist.sync();
+
+module.exports = {
+  Artist: Artist
+};
