58da6b6b57d189422605869060ad9a087e2d6ab4
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-17 10:33:05 -0700

    create express server

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = process.env.PORT || 1337;
+
+app.listen(port, () => {
+  console.log(`I am listening to port ${port}`)
+});
