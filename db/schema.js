ef126a6a41b25ff4738229bd3190ce3ed022f8a7
woonandez <matthew.allen.fernandez@gmail.com>
2017-10-17 10:36:30 -0700

    add sequelize orm

---
+++ db/schema.js
@@ -0,0 +1,2 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('hiphop', 'root', '');
--- index.js
+++ index.js
@@ -6,5 +6,5 @@ var app = express();
 var port = process.env.PORT || 1337;
 
 app.listen(port, () => {
-  console.log(`I am listening to port ${port}`)
+  console.log(`I am listening to port ${port}`);
 });
